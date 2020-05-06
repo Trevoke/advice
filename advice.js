3ef317834bee54a376ea2199375486e1afc161a8
Trevoke <Trevoke@users.noreply.github.com>
2020-05-05 22:19:01 -0400

    Update advice.js

--- advice.js
+++ advice.js
@@ -1,25 +1,11 @@
-function mulberry32(a) {
-    return function() {
-      var t = a += 0x6D2B79F5;
-      t = Math.imul(t ^ t >>> 15, t | 1);
-      t ^= t + Math.imul(t ^ t >>> 7, t | 61);
-      return ((t ^ t >>> 14) >>> 0) / 4294967296;
-    }
-}
-
 var advice = [
   'Advice 1',
   'Advice 2',
   'Advice 3'
 ];
 
-var date = new Date();
-var year = String(date.getFullYear());
-var month = String(date.getMonth()).padStart(2, '0');
-var day = String(date.getDay()).padStart(2, '0');
+var dayTimestamp = new Date() / 1000;
 
-var seed = year + month + day;
+var index = dayTimestamp % advice.length;
 
-adviceIndex = Math.floor(mulberry32(seed)() * advice.length);
-
-console.log(advice[adviceIndex]);
+console.log(advice[index]);
